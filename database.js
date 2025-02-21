b24f0c7eef27c91884891649134ec19dd442d1bc
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2025-02-21 12:49:32 +0100

    Update database.js

--- database.js
+++ database.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '0000',
-  database: 'chatdb',
+  host: 'mysql-achked.alwaysdata.net',       // adjust if necessary
+  user: 'achked',            // your MySQL username
+  password: 'kedata2003@@@', // your MySQL password
+  database: 'achked_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
